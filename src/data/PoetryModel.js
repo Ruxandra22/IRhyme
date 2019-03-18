99cf7ef6ffda00ae08e73529e666f3c3890fe69a
Kata-M <martikai@kth.se>
2019-03-18 17:02:31 +0100

    merging prep with ruxi

--- src/data/PoetryModel.js
+++ src/data/PoetryModel.js
@@ -1,27 +1,25 @@
 import ObservableModel from "./ObservableModel";
 import API_KEY_PHOTOS from "./ApiKey";
 
-const BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com";
+const SELECTTHEME_BASE_URL= "https://api.pexels.com/v1";
 const SELECTCARD_BASE_URL = "https://api.pexels.com/v1/search?per_page=2&page=1";
-const httpOptionsSelectCard = {
-  headers: { "Authorization": API_KEY_PHOTOS}
-};
 const httpOptions = {
-  headers: { "Authorization": API_KEY_PHOTOS}
+ headers: { "Authorization": API_KEY_PHOTOS}
 };
 
 class PoetryModel extends ObservableModel {
+  themes = ["Birthday", "Wedding", "Love", "Travel", "Mother's Day", "Christmas"];
+
   constructor() {
     super();
-    this._numberOfGuests = 4;
-    this.getNumberOfGuests();
     this.cardImage; 
+    this.urlThemePhotosList = [];
   }
 
 //method to get cards according to the selected theme
 getSelectCards(query){
   const url = `${SELECTCARD_BASE_URL}&query=${query}`;
-  return fetch(url, httpOptionsSelectCard).then(this.processResponse);
+  return fetch(url, httpOptions).then(this.processResponse);
 }
 
 //method to set the card image what the user wants to use
@@ -35,32 +33,14 @@ getCardImage(){
   return this.cardImage;
 }
 
-  /**
-   * Get the number of guests
-   * @returns {number}
-   */
-  getNumberOfGuests() {
-    return this._numberOfGuests;
+//From Ruxi
+  getUrlThemePhotosList() {
+      return this.urlThemePhotosList;
   }
 
-  /**
-   * Set number of guests
-   * @param {number} num
-   */
-  setNumberOfGuests(num) {
-    this._numberOfGuests = num;
-    this.notifyObservers();
-  }
-
-  // API methods
-
-  /**
-   * Do an API call to the search API endpoint.
-   * @returns {Promise<any>}
-   */
-  getAllDishes() {
-    const url = `${BASE_URL}/recipes/search`;
-    return fetch(url, httpOptions).then(this.processResponse);
+  getThemePhoto(theme) {
+      const url = `${SELECTTHEME_BASE_URL}/search?query=` + theme + `&per_page=1&page=1`;
+      return fetch(url, httpOptions).then(this.processResponse);
   }
 
   processResponse(response) {
