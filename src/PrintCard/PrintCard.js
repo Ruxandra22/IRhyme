243f6760faf449b10b1542b16327ae90e9d19b90
SSRao <sindhusrao@gmail.com>
2019-04-16 11:48:31 +0200

    A4 size print

--- src/PrintCard/PrintCard.js
+++ src/PrintCard/PrintCard.js
@@ -8,9 +8,50 @@ import{Row, Col} from 'reactstrap';
 import Button from "react-bootstrap/Button";
 import firebase from "../config/dbConfig";
 import img3 from '../images/Overlay.jpg';
+import ReactToPrint from "react-to-print";
 
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
+
+
+class PrintFront extends Component {  
+
+  render() {
+
+      return(
+          <div className="row">
+                  <div className="col-12" style={{ backgroundImage: 'url(' + require('../images/A4.jpg') + ')', backgroundRepeat: 'no-repeat',  backgroundPosition: 'center'}}>
+                              <img className="figureImg11" src={this.props.url}/>
+                  </div> 
+          </div>
+
+      );
+  }
+
+}
+
+class PrintInside extends Component {  
+
+render() {
+
+    return(
+        <div className="row">
+                  <div className="row" style={{ backgroundImage: 'url(' + require('../images/A4.jpg') + ')', backgroundRepeat: 'no-repeat',  backgroundPosition: 'center'}}>
+                      <div className="col-6">       
+                              <img className="figureImg12" src={this.props.url}/>
+                      </div>  
+                      <div className="col-6 mt-5">      
+                          <strong className="text-primary">Dear Friend</strong>
+                              <p className="pr-5">{this.props.cardTxt}</p>
+                          <strong className="text-primary">Best Wishes</strong>
+                      </div>       
+                  </div>
+        </div>
+
+    );
+}
+
+}
 
 
 class PrintCard extends Component {
@@ -50,6 +91,7 @@ class PrintCard extends Component {
           // })
 
       }
+
 
     addCard = e => {
         e.preventDefault();
@@ -207,6 +249,38 @@ class PrintCard extends Component {
 
               </div>
             {/*<Button onClick={this.addCard} variant="outline-info">Save Card</Button>*/}
+
+
+            <div class="container">
+                  <div className="row justify-content-center align-items-center">
+                      <h2 >Print Card</h2> 
+                  </div>    
+                  <div className="row justify-content-center align-items-center">    
+                      <p>Use A5 size photo paper to print this card.</p>
+                  </div>
+
+                <div className="row justify-content-center align-items-center">
+                  <PrintFront url={this.state.url} cardTxt={this.state.cardTxt} ref={el1 => (this.componentRef1 = el1)} />
+                </div>
+                <div className="row justify-content-center align-items-center">   
+                  <ReactToPrint
+                    trigger={() => <button className="PrintBtn">Print card!</button>}
+                    content={() => this.componentRef1}
+                  />
+                </div>
+
+                <div className="row justify-content-center align-items-center">
+                  <PrintInside url={this.state.url} cardTxt={this.state.cardTxt} ref={el2 => (this.componentRef2 = el2)} />
+                </div>
+                <div className="row justify-content-center align-items-center">  
+                  <ReactToPrint
+                    trigger={() => <button className="PrintBtn">Print Inside!</button>}
+                    content={() => this.componentRef2}
+                  />
+                </div>
+
+            </div>      
+
            </div>
 
         );
